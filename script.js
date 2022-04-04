bc0a9507e13516a0e069472b6dcf0d6d7a294a28
sevass <sevarakodirova7711@gmail.com>
2022-04-04 09:40:38 +0500

    hdjkbe

---
+++ script.js
@@ -0,0 +1,62 @@
+// let studentNames = [prompt('enter the names')]
+// // let names = [studentNames]
+
+
+
+// for (let i = 0; i < studentNames.length; i++) {
+//     const element = studentNames[i];
+    
+// }
+// if (studentNames == 'stop', 'STOP','Stop') {
+//     alert('your students list is ready')
+// }else{
+//     studentNames + i
+// }
+
+// do {
+//     alert('your students list is ready')
+// } while (studentNames == 'stop');
+
+// if (studentNames == 'Stop', 'stop', 'STOP') {
+//     alert('your students list is ready')
+// }
+
+// while(studentNames !== i){
+//     prompt('enter the names')
+// }
+
+// if (studentNames == i ) {
+//     alert('ready')
+// }
+ 
+
+// console.log(studentNames);
+
+// if (studentNames == 'stop'|| 'STOP'||'Stop') {
+//         alert('your students list is ready')
+//     }else{
+//         console.log(element);
+//    
+
+
+let names = []
+
+
+for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+    
+}
+
+
+
+while (true) {
+    names = [prompt("Enter a name:")]
+    if (names == "stop") {
+        alert('your list is ready');
+        break;
+    }
+
+    console.log(names);
+}
+
+ 
